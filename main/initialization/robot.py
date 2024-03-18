68bccedf04059b7bf80176e34f310d55b80c7d6f
Pedro (Peter) Palacios Jr <peterpalaciosjr9@gmail.com>
2024-03-18 13:14:31 -0500

    Constants file created for organizational purposes of code and easy accessibility of drivetrain constants

--- main/initialization/robot.py
+++ main/initialization/robot.py
@@ -1,11 +1,13 @@
-import wpilib
-import wpilib.drive
+from wpilib import drive, SmartDashboard, Timer
 from rev import CANSparkMax, CANSparkBase, MotorType
+import constants
 
 class MyRobot(wpilib.TimedRobot):
 
     def robotInit(self):
         def robot_base():
+            const = constants.Constants
+
             LEFT_FRONT, LEFT_REAR = CANSparkMax(1, MotorType.kBrushless), CANSparkMax(2, MotorType.kBrushless)
             RIGHT_FRONT, RIGHT_REAR = CANSparkMax(3, MotorType.kBrushless), CANSparkMax(4, MotorType.kBrushless)
 
@@ -13,20 +15,16 @@ class MyRobot(wpilib.TimedRobot):
             CANSparkMax(7, MotorType.kBrushless)
             CLIMBER = CANSparkMax(8, MotorType.kBrushless)
 
-            DIFFERENTIAL_DRIVE_CURRENT = 100
-            LAUNCH_WHEEL_CURRENT, FEEDER_WHEEL_CURRENT, ROLLER_CLAW_CURRENT = 145, 145, 100
-            CLIMBER_CURRENT = 100
+            LEFT_FRONT.CANSparkBase.setSmartCurrentLimit(const.DIFFERENTIAL_DRIVE_CURRENT)
+            LEFT_REAR.CANSparkBase.setSmartCurrentLimit(const.DIFFERENTIAL_DRIVE_CURRENT)
+            RIGHT_FRONT.CANSparkBase.setSmartCurrentLimit(const.DIFFERENTIAL_DRIVE_CURRENT)
+            RIGHT_REAR.CANSparkBase.setSmartCurrentLimit(const.DIFFERENTIAL_DRIVE_CURRENT)
 
-            LEFT_FRONT.CANSparkBase.setSmartCurrentLimit(DIFFERENTIAL_DRIVE_CURRENT)
-            LEFT_REAR.CANSparkBase.setSmartCurrentLimit(DIFFERENTIAL_DRIVE_CURRENT)
-            RIGHT_FRONT.CANSparkBase.setSmartCurrentLimit(DIFFERENTIAL_DRIVE_CURRENT)
-            RIGHT_REAR.CANSparkBase.setSmartCurrentLimit(DIFFERENTIAL_DRIVE_CURRENT)
+            LAUNCH_WHEEL.CANSparkBase.setSmartCurrentLimit(const.LAUNCH_WHEEL_CURRENT)
+            FEEDER_WHEEL.CANSparkBase.setSmartCurrentLimit(const.FEEDER_WHEEL_CURRENT)
+            ROLLER_CLAW.CANSparkBase.setSmartCurrentLimit(const.ROLLER_CLAW_CURRENT)
 
-            LAUNCH_WHEEL.CANSparkBase.setSmartCurrentLimit(LAUNCH_WHEEL_CURRENT)
-            FEEDER_WHEEL.CANSparkBase.setSmartCurrentLimit(FEEDER_WHEEL_CURRENT)
-            ROLLER_CLAW.CANSparkBase.setSmartCurrentLimit(ROLLER_CLAW_CURRENT)
-
-            CLIMBER.CANSparkBase.setSmartCurrentLimit(CLIMBER_CURRENT)
+            CLIMBER.CANSparkBase.setSmartCurrentLimit(const.CLIMBER_CURRENT)
 
             LEFT_REAR.CANSparkBase.follow(LEFT_FRONT)
             RIGHT_REAR.CANSparkBase.follow(RIGHT_FRONT)
@@ -40,23 +38,25 @@ class MyRobot(wpilib.TimedRobot):
             ROLLER_CLAW.CANSparkBase.burnFlash()
             CLIMBER.CANSparkBase.burnFlash()
         
-        def joystick_init(type1, type2):
-            driver_controller_type, operator_controller_type = type1, type2
-
+        def joystick_init(driver_controller_type, operator_controller_type):
             self.driver_joystick = wpilib.PS4Controller(0) if (driver_controller_type) == "PS4" else (wpilib.Joystick(0))
             self.operator_joystick = wpilib.PS4Controller(1) if (operator_controller_type) == "PS4" else (wpilib.Joystick(1))
+
+
 
         robot_base()
         joystick_init("PS4", "PS4")
 
+    def robotPeriodic(self):
+        current_time = Timer.getFPGATimestamp()
+        SmartDashboard.putNumber("Current runtime for robot (s):", current_time)
 
     def teleopPeriodic(self):
         pass
 
     def autonomousInit(self):
-        pass
+        autonomous_start = Timer.getFPGATimestamp()
     
     def autonomousPeriodic(self):
+        # Autonomous primary commands
         pass
-
-    
