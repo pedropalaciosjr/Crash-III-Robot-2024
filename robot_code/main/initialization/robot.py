a72eee3ca89e580e5884970dad3ecf9ed4853af3
Pedro (Peter) Palacios Jr <peterpalaciosjr9@gmail.com>
2024-03-19 11:30:29 -0500

    Brownout & temperature SmartDashboard statements written for output.

--- robot_code/main/initialization/robot.py
+++ robot_code/main/initialization/robot.py
@@ -20,7 +20,15 @@ class MyRobot(TimedRobot):
             self.LEFT_FRONT, self.LEFT_REAR = rev.CANSparkMax(const.LEFT_FRONT_CAN_ID, rev.MotorType.kBrushless), rev.CANSparkMax(const.LEFT_REAR_CAN_ID, rev.MotorType.kBrushless)
             self.RIGHT_FRONT, self.RIGHT_REAR = rev.CANSparkMax(const.RIGHT_FRONT_CAN_ID, rev.MotorType.kBrushless), rev.CANSparkMax(const.RIGHT_REAR_CAN_ID, rev.MotorType.kBrushless)
 
-            self.SPARKMAX_CONTROLLERS = [self.LEFT_FRONT, self.LEFT_REAR, self.RIGHT_FRONT, self.RIGHT_REAR]
+            self.SPARKMAX_CONTROLLERS = [
+                self.LEFT_FRONT, 
+                self.LEFT_REAR, 
+                self.RIGHT_FRONT, 
+                self.RIGHT_REAR, 
+                self.LAUNCH_WHEEL, 
+                self.FEEDER_WHEEL, 
+                self.ROLLER_CLAW
+            ]
 
             self.LAUNCH_WHEEL, self.FEEDER_WHEEL, self.ROLLER_CLAW = rev.CANSparkMax(const.LAUNCH_WHEEL_CAN_ID, rev.MotorType.kBrushless), rev.CANSparkMax(const.FEEDER_WHEEL_CAN_ID, rev.MotorType.kBrushless),\
                 rev.CANSparkMax(const.ROLLER_CLAW_CAN_ID, rev.MotorType.kBrushless)
@@ -70,25 +78,34 @@ class MyRobot(TimedRobot):
                 motor_temperatures.append((1.8 * sparkmax.getMotorTemperature()) + 32)
                 brownout_faults.append(sparkmax.getFault(0))
             
-            left_front_motor_temperature, left_rear_motor_temperature, right_front_motor_temperature, right_rear_motor_temperature = motor_temperatures
-            left_front_brownout = 
-            SmartDashboard.putNumber(f"Left Front Motor Temperature (F) {left_front_motor_temperature}")
-            SmartDashboard.putNumber(f"Left Rear Motor Temperature (F) {left_rear_motor_temperature}")
-            SmartDashboard.putNumber(f"Right Front Motor Temperature (F) {right_front_motor_temperature}")
-            SmartDashboard.putNumber(f"Right Rear Motor Temperature (F) {right_rear_motor_temperature}")
+            left_front_motor_temperature, left_rear_motor_temperature, right_front_motor_temperature, right_rear_motor_temperature,\
+                launch_wheel_temperature, feeder_wheel_temperature, roller_claw_temperature, climber_temperature = motor_temperatures
+            left_front_brownout, left_rear_brownout, right_front_brownout, right_rear_brownout, launch_wheel_brownout,\
+                 feeder_wheel_brownout, roller_claw_brownout, climber_brownout = brownout_faults
+            
+            SmartDashboard.putNumber("Left Front Motor Temperature (F)", left_front_motor_temperature)
+            SmartDashboard.putNumber("Left Rear Motor Temperature (F)", left_rear_motor_temperature)
+            SmartDashboard.putNumber("Right Front Motor Temperature (F)", right_front_motor_temperature)
+            SmartDashboard.putNumber("Right Rear Motor Temperature (F)", right_rear_motor_temperature)
+            SmartDashboard.putNumber("Launch Wheel Motor Temperature (F)", launch_wheel_temperature)
+            SmartDashboard.putNumber("Feeder Wheel Motor Temperature (F)", feeder_wheel_temperature)
+            SmartDashboard.putNumber("Roller Claw Motor Temperature (F)", roller_claw_temperature)
 
-
-            SmartDashboard.putBoolean(f"Left Front Brownout Detected: {brownout_faults}")
-            SmartDashboard.putBoolean(f"Left Rear Brownout Detected: {True}")
-            SmartDashboard.putBoolean(f"Right Front Brownout Detected: {True}")
-            SmartDashboard.putBoolean(f"Right Rear Brownout Detected: {True}")
+            SmartDashboard.putBoolean("Left Front Brownout Detected:", left_front_brownout)
+            SmartDashboard.putBoolean("Left Rear Brownout Detected:", left_rear_brownout)
+            SmartDashboard.putBoolean("Right Front Brownout Detected:", right_front_brownout)
+            SmartDashboard.putBoolean("Right Rear Brownout Detected:", right_rear_brownout)
+            SmartDashboard.putBoolean("Launch Wheel Brownout Detected:", launch_wheel_brownout)
+            SmartDashboard.putBoolean("Feeder Wheel Brownout Detected:", feeder_wheel_brownout)
+            SmartDashboard.putBoolean("Roller Claw Brownout Detected:", roller_claw_brownout)
+            SmartDashboard.putBoolean("Climber Brownout Detected:", climber_brownout)
 
         robot_base()
         joystick_init(const.driver_controller_type, const.operator_controller_type)
 
     def robotPeriodic(self):
         current_time = Timer.getFPGATimestamp()
-        SmartDashboard.putNumber(f"Robot Runtime (seconds): {current_time}")
+        SmartDashboard.putNumber(f"Robot Runtime (seconds):, current_time}")
 
 
 
